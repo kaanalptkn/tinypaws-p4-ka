dfff22a872a461a204c229ffe86f3828beed96ed
Kaan Alptakin <kaanalptkn@icloud.com>
2022-05-29 10:21:27 +0000

    Added prdoucts, templates, details

---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.all_products, name='product_detail'),
+]
