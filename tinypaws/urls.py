8c48a6108b3efb09f0e4cecd76ae8872c077fc02
Kaan Alptakin <kaanalptkn@icloud.com>
2022-06-14 11:50:32 +0000

    Created profile app

--- tinypaws/urls.py
+++ tinypaws/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
