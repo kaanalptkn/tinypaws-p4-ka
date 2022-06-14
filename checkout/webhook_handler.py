6cdc3b374ddc59552583fc9c213cd5e41e7960c3
Kaan Alptakin <kaanalptkn@icloud.com>
2022-06-14 11:29:59 +0000

    Updated checkout form

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -51,7 +51,6 @@ class StripeWH_Handler:
                     town_or_city__iexact=shipping_details.address.city,
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
-                    county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_basket=basket,
                     stripe_pid=pid,
@@ -77,7 +76,6 @@ class StripeWH_Handler:
                     town_or_city=shipping_details.address.city,
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
-                    county=shipping_details.address.state,
                     original_basket=basket,
                     stripe_pid=pid,
                 )
