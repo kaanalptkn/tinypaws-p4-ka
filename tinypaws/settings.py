82a4d40934897500eae8a1258a18c4ab04a08421
Kaan Alptakin <kaanalptkn@icloud.com>
2022-07-02 12:47:29 +0000

    Added basic deployment

--- tinypaws/settings.py
+++ tinypaws/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -126,7 +127,7 @@ WSGI_APPLICATION = 'tinypaws.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
