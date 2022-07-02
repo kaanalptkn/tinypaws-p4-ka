d5261200ef82c42f7865e0f13369e84cf5cb39d0
Kaan Alptakin <kaanalptkn@icloud.com>
2022-07-02 13:12:05 +0000

    Added deployment for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tinypaws.wsgi:application
--- tinypaws/settings.py
+++ tinypaws/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0m(&$&cw3=aqaxfv8e!=r0o9_y08#9ro)2lpe-2-)vnhnz+i@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://git.heroku.com/kaan707-tiny-paws.git', 'localhost']
 
 
 # Application definition
@@ -124,13 +124,17 @@ WSGI_APPLICATION = 'tinypaws.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
